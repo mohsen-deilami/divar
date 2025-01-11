c930df45d34e36fd1fb06166665aed89c4b132de
Mohsen Deilami <mohsen.deilami6570@gmail.com>
2025-01-12 00:19:34 +0100

    category list

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -1,13 +1,22 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react'
 import { getCategories } from '../services/services';
+import { Container } from '@mui/material';
+import Grid from "@mui/material/Grid2";
 
 export default function CategoryList() {
     const {data , isLoading , error}=useQuery({queryKey:["get-category"],queryFn:()=>getCategories()});
      console.log(data , error)
   return (
-    <div>
-      
-    </div>
+    <Container maxWidth="lg">
+      {isLoading ? <p>Loading...</p> :''}
+     {data && data.data.map(item =>(
+
+<Grid key={item._id}>
+<h5>{item.name}</h5>
+<p>{item.slug}</p>
+</Grid>        
+     ))}
+    </Container>
   )
 }
