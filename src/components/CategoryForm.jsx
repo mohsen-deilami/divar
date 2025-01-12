757bced45e40f4064e3a815e05fc7569fc860f01
Mohsen Deilami <mohsen.deilami6570@gmail.com>
2025-01-12 13:27:32 +0100

    finished category page

--- src/components/CategoryForm.jsx
+++ src/components/CategoryForm.jsx
@@ -1,108 +1,121 @@
 import React, { useState } from "react";
-import {  Button, Container,  TextField, Typography  , Select} from "@mui/material";
+import {
+  Button,
+  Container,
+  TextField,
+  Typography,
+  Select,
+} from "@mui/material";
 import Grid from "@mui/material/Grid2";
 import { addCategory } from "../services/services";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { toast, ToastContainer } from "react-toastify";
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
 
 export default function CategoryForm() {
+  const [icon, setIcon] = useState("");
+  const queryClient=useQueryClient();
+  const [form, setForm] = useState({ name: "", slug: "", icon: "" });
+  const handleChange = (event) => {
+    setIcon(event.target.value);
+    setForm({ ...form, [event.target.name]: event.target.value }, icon);
+    console.log(form);
+  };
 
-
-    const [icon, setIcon] = useState('');
-
-    const handleChange = (event) => {
-        setIcon(event.target.value);
-        setForm({...form , [event.target.name]:event.target.value}, icon);
-        console.log(form)
-    };
-
-    const [form , setForm]=useState({name:"",slug:"",icon:""});
-    const {mutate , isLoading , error, data}=useMutation({mutationFn: addCategory})
-    const changeHandler=event=>{
-        setForm({...form , [event.target.name]:event.target.value});
-        console.log(form)
-    }
-    const submitHandler=event=>{
-        event.preventDefault();
-        mutate(form ,{onSuccess : data =>console.log(data) , onError : error => console.log(error)})
-        console.log(form);
-        
-    }
+  const { mutate, isLoading, error, data } = useMutation({
+    mutationFn: addCategory,
+  });
+  const changeHandler = (event) => {
+    setForm({ ...form, [event.target.name]: event.target.value });
+    console.log(form);
+  };
+  const submitHandler = (event) => {
+    event.preventDefault();
+    mutate(form, {
+      onSuccess: (data) => queryClient.invalidateQueries({
+        queryKey:["get-category"]
+      }),
+      onError: (error) => console.log(error),
+    });
+    console.log(form);
+  };
   return (
     <Container maxWidth="lg">
-      <form style={{marginTop:'32px'}} onChange={changeHandler} onSubmit={submitHandler}>
-     
-
-        <Typography variant="h4" component="h4" sx={{ marginTop: "10px" , display:'block'}}>
+      <form
+        style={{ marginTop: "32px" }}
+        onChange={changeHandler}
+        onSubmit={submitHandler}
+      >
+        <Typography
+          variant="h4"
+          component="h4"
+          sx={{ marginTop: "10px", display: "block" }}
+        >
           Enter new Category
         </Typography>
-        {data?.status === 201 && 
-        (
-         toast.success("New category successfully registered.", {
-                  position: "top-center",
-                })
-            )
+        {data?.status === 201 &&
+          toast.success("New category successfully registered.", {
+            position: "top-center",
+          })}
 
-        }
+        {!!error &&
+          toast.warn("There was a problem with the program.", {
+            position: "top-center",
+          })}
 
-        {!!error && (
-         toast.warn("There was a problem with the program.", {
-                  position: "top-center",
-                })
-            )
-          
-        }
-
-      <Grid sx={{ display:'block' , marginTop:'20px' }}>
-
-
-        <TextField sx={{ width:'300px'}}
-          type="text"
-          label="Enter the name number..."
-          variant="outlined"
-          name="name"
-          id="name"
+        <Grid sx={{ display: "block", marginTop: "20px" }}>
+          <TextField
+            sx={{ width: "300px" }}
+            type="text"
+            label="Enter the name number..."
+            variant="outlined"
+            name="name"
+            id="name"
           />
-          </Grid>
+        </Grid>
 
-          <Grid sx={{ display:'block' , marginTop:'10px' }}>
-        <TextField sx={{ width:'300px'}}
-          type="text"
-          label="Enter the slug..."
-          variant="outlined"
-          name="slug"
-          id="slug"
-
+        <Grid sx={{ display: "block", marginTop: "10px" }}>
+          <TextField
+            sx={{ width: "300px" }}
+            type="text"
+            label="Enter the slug..."
+            variant="outlined"
+            name="slug"
+            id="slug"
           />
-          </Grid>
-       
-          <Grid sx={{ display:'block' , marginTop:'10px' }}> 
-      <FormControl sx={{ minWidth: '300px' }}>
-        <InputLabel id="demo-simple-select-label">Icon</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="icon"
-          name='icon'
-          value={icon}
-          label="Icon"
-          onChange={handleChange}
+        </Grid>
+
+        <Grid sx={{ display: "block", marginTop: "10px" }}>
+          <FormControl sx={{ minWidth: "300px" }}>
+            <InputLabel id="demo-simple-select-label">Icon</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="icon"
+              name="icon"
+              value={icon}
+              label="Icon"
+              onChange={handleChange}
+            >
+              <MenuItem value={"game"}>game</MenuItem>
+              <MenuItem value={"home"}>home</MenuItem>
+              <MenuItem value={"personal"}>personal</MenuItem>
+              <MenuItem value={"profiile"}>profiile</MenuItem>
+              <MenuItem value={"service"}>service</MenuItem>
+              <MenuItem value={"digital"}>Digital</MenuItem>
+              <MenuItem value={"car"}>Car</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Button
+          variant="outlined"
+          style={{ width: "140px", marginTop: "10px", marginBottom: "20px" }}
+          onClick={submitHandler}
         >
-          <MenuItem value={'game'}>game</MenuItem>
-          <MenuItem value={'home'}>home</MenuItem>
-          <MenuItem value={'personal'}>personal</MenuItem>
-          <MenuItem value={'profiile'}>profiile</MenuItem>
-          <MenuItem value={'service'}>service</MenuItem>
-        </Select>
-      </FormControl>
-      </Grid>
-        <Button variant="outlined"  style={{width:'140px' , marginTop:'10px' , marginBottom:'20px'}} onClick={submitHandler}>
-        Create...
-      </Button>
-      <ToastContainer />
+          Create...
+        </Button>
+        <ToastContainer />
       </form>
     </Container>
   );
--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -1,22 +1,55 @@
-import { useQuery } from '@tanstack/react-query';
-import React from 'react'
-import { getCategories } from '../services/services';
-import { Container } from '@mui/material';
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import { getCategories } from "../services/services";
+import { Container, Typography } from "@mui/material";
 import Grid from "@mui/material/Grid2";
 
 export default function CategoryList() {
-    const {data , isLoading , error}=useQuery({queryKey:["get-category"],queryFn:()=>getCategories()});
-     console.log(data , error)
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["get-category"],
+    queryFn: () => getCategories(),
+  });
+  console.log(data, error);
   return (
     <Container maxWidth="lg">
-      {isLoading ? <p>Loading...</p> :''}
-     {data && data.data.map(item =>(
+      {isLoading ? <p>Loading...</p> : ""}
+      {data &&
+        data.data.map((item) => (
+          <Grid
+            key={item._id}
+            sx={{
+              border: "2px solid #eaeaea",
+              margin: "20px 0",
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems:'center',
+              padding: "10px 20px",
+            }}
+          >
+            <Grid sx={{display:'flex' , alignItems:'center' , gap:'16px'}}>
 
-<Grid key={item._id}>
-<h5>{item.name}</h5>
-<p>{item.slug}</p>
-</Grid>        
-     ))}
+            <img
+              src={`${item.icon}.svg`}
+              alt=""
+              style={{ height: "25px", width: "25px" }}
+              />
+            <Typography
+              variant="h5"
+              component="h4"
+              sx={{ marginTop: "10px", display: "block" }}
+              >
+              {item.name}
+            </Typography>
+            </Grid>
+            <Typography
+              variant="p"
+              component="p"
+              sx={{ marginTop: "10px", display: "block" }}
+              >
+              slug : {item.slug}
+            </Typography>
+          </Grid>
+        ))}
     </Container>
-  )
+  );
 }
