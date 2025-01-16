5545cd06f994827ca2aa51f3cbe8836c58396cec
Mohsen Deilami <mohsen.deilami6570@gmail.com>
2025-01-16 22:26:26 +0100

    update homepage

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -16,13 +16,13 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import ShareIcon from "@mui/icons-material/Share";
 import MoreVertIcon from "@mui/icons-material/MoreVert";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import { ExpandMore } from "@mui/icons-material";
+import { ExpandMore, Margin } from "@mui/icons-material";
 export default function Main({ posts, categories }) {
   return (
-    <Container maxWidth='lg' style={{display:'flex' , alignItems:'center' , flex:'1'}}>
+    <Container maxWidth='lg' sx={{display: 'flex',  alignItems: 'center', justifyContent: 'space-between',  flexWrap: 'wrap' , marginLeft:'32px'}}>
       {posts && categories
         ? posts.data.posts.map((post) => (
-            <Grid style={{ border:'2px solid #eaeaea' , borderRadius:'8px' , padding:'5px'}} key={post._id}>
+            <Grid style={{width:'340px', border:'2px solid #eaeaea' , borderRadius:'8px' , padding:'5px' , marginTop:'16px'}} key={post._id}>
              
               {categories.data.map(
                 (category) =>
@@ -56,53 +56,17 @@ export default function Main({ posts, categories }) {
               />
               <CardContent>
                 <Typography variant="body2" sx={{ color: "text.secondary" }}>
-                  {post.options.content}
+                  {post.options.content?.slice(0 , 10)}
                 </Typography>
               </CardContent>
               <CardActions disableSpacing>
-                <IconButton aria-label="add to favorites">
-                  <FavoriteIcon />
-                </IconButton>
-                <IconButton aria-label="share">
-                  <ShareIcon />
-                </IconButton>
+              
+               
                 <ExpandMore aria-label="show more">
                   <ExpandMoreIcon />
                 </ExpandMore>
               </CardActions>
-              <Collapse timeout="auto" unmountOnExit>
-                <CardContent>
-                  <Typography sx={{ marginBottom: 2 }}>Method:</Typography>
-                  <Typography sx={{ marginBottom: 2 }}>
-                    Heat 1/2 cup of the broth in a pot until simmering, add
-                    saffron and set aside for 10 minutes.
-                  </Typography>
-                  <Typography sx={{ marginBottom: 2 }}>
-                    Heat oil in a (14- to 16-inch) paella pan or a large, deep
-                    skillet over medium-high heat. Add chicken, shrimp and
-                    chorizo, and cook, stirring occasionally until lightly
-                    browned, 6 to 8 minutes. Transfer shrimp to a large plate
-                    and set aside, leaving chicken and chorizo in the pan. Add
-                    pimentón, bay leaves, garlic, tomatoes, onion, salt and
-                    pepper, and cook, stirring often until thickened and
-                    fragrant, about 10 minutes. Add saffron broth and remaining
-                    4 1/2 cups chicken broth; bring to a boil.
-                  </Typography>
-                  <Typography sx={{ marginBottom: 2 }}>
-                    Add rice and stir very gently to distribute. Top with
-                    artichokes and peppers, and cook without stirring, until
-                    most of the liquid is absorbed, 15 to 18 minutes. Reduce
-                    heat to medium-low, add reserved shrimp and mussels, tucking
-                    them down into the rice, and cook again without stirring,
-                    until mussels have opened and rice is just tender, 5 to 7
-                    minutes more. (Discard any mussels that don&apos;t open.)
-                  </Typography>
-                  <Typography>
-                    Set aside off of the heat to let rest for 10 minutes, and
-                    then serve.
-                  </Typography>
-                </CardContent>
-              </Collapse>
+            
             </Grid>
 
             /* <Grid key={post._id} sx={{display:'flex', justifyContent:'space-between', border:'2px solid #eaeaea' , borderRadius:'8px' , padding:'5px'}}>
